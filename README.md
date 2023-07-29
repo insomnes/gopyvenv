10f460909f8cbc6ac55a8ddf9e012c11d219ca9e
insomnes <denginm@gmail.com>
2023-07-29 19:03:52 +0200

    Add bash support

--- README.md
+++ README.md
@@ -5,19 +5,20 @@ Works with `venv` and `.venv` directories.
 
 # Install
 ## Auto
-You can run `install.sh` or `make install` for autpinstallation. This will install `gopyvenv` to `$HOME/.pyenv` and add include to your `.zshrc` file with neccessary hooks.
+You can run `install.sh` or `make install` for auto-installation. This will install `gopyvenv` to `$HOME/.pyenv` and add include to your `.bashrc` or `.zshrc` file with necessary hooks. 
 
 ## Manual
 You can also build the file with `make build` or `go build -o bin/ cmd/gopyvenv/gopyvenv.go` and copy from `bin/gopyvenv` somewhere inside your `$PATH`.
 
-Hooks and neede environment variables can be found in `config/zsh.inc.dist` 
+Hooks and needed environment variables can be found in `config/zsh.inc.dist` or `config/bash.inc.dist`
 
 # How it works?
 Binary will return specific command on new instances of zsh or change directory
 based on current venv status.
 
 ## No venv is active
-If you have `venv` or `.venv` subdir it will be activated.
+If you have `venv` or `.venv` subdir it will be activated. Dir list is configurable by `GOPYVENV_DIR_NAMES` env var.
+
 ## Venv is active
-Going to child directory will not deactivate, only parent or totally differnt one will 
+Going to child directory will not deactivate, only parent or totally different one will 
 cause deactivation. 
--- install.sh
+++ install.sh
@@ -2,15 +2,25 @@
 
 GOPYVENV_ROOT="$HOME/.gopyvenv"
 GOPYVENV_BIN="$GOPYVENV_ROOT/bin"
-GOPYVENV_INC="$GOPYVENV_ROOT/zsh.inc"
 
+USER_SHELL="$(echo "$SHELL" | rev | cut -d '/' -f1 | rev)"
+
+
+if [[ $USER_SHELL != "zsh" ]] && [[ $USER_SHELL != "bash" ]]; then
+    echo "Unsupported shell for autoinstall: $SHELL"
+    exit 1
+fi
+
+
+GOPYVENV_INC="$GOPYVENV_ROOT/$USER_SHELL.inc"
+RC_FILE="$HOME/.${USER_SHELL}rc"
 
 COMMENT_STRING="# GOPYVENV AUTOINSTALL DO NOT CHANGE THIS LINE OR COMMENT"
 INCLUDE_STRING="if [ -f '$GOPYVENV_INC' ]; then . '$GOPYVENV_INC';fi"
 FULL_INCLUDE="$INCLUDE_STRING  $COMMENT_STRING"
 
 
-RELOAD_CMD="source $HOME/.zshrc"
+RELOAD_CMD="source $RC_FILE"
 
 echo "Creating $GOPYVENV_ROOT if needed"
 mkdir -p "$GOPYVENV_BIN"
@@ -18,19 +28,19 @@ mkdir -p "$GOPYVENV_BIN"
 echo "Building ..."
 go build -o "$GOPYVENV_BIN/" cmd/gopyvenv/gopyvenv.go
 
-echo "Making $HOME/.zshrc backup at $HOME/.zshrc.pre_gopyvenv.bcp"
-cp "$HOME/.zshrc" "$HOME/.zshrc.pre_gopyvenv.bcp"
+echo "Making $RC_FILE backup at $RC_FILE.pre_gopyvenv.bcp"
+cp "$RC_FILE" "$RC_FILE.pre_gopyvenv.bcp"
 
 echo "Setting include"
 
 if ! [[ -f "$GOPYVENV_INC" ]]; then
-    cp ./config/zsh.inc.dist "$GOPYVENV_INC"
+    cp "./config/${USER_SHELL}.inc.dist" "$GOPYVENV_INC"
 fi
 
-INCLUDE_FOUND=$(grep "$COMMENT_STRING" "$HOME/.zshrc")
+INCLUDE_FOUND=$(grep "$COMMENT_STRING" "${RC_FILE}")
 if [[ $INCLUDE_FOUND == "" ]] ; then
     echo "Include not found, adding"
-    echo "$FULL_INCLUDE" >> "$HOME/.zshrc"
+    echo "$FULL_INCLUDE" >> "$RC_FILE"
 fi
 
 echo "Finished gopyvenv installation, reload your profile with:"
